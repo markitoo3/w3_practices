d877e3549b842d066862e12f2edb83285705db34
markitoo3 <dietrich.marko@gmail.com>
2022-02-03 17:56:43 +0100

    script és html headbar

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-function addTwoIntegers(firstInteger, secondInteger) {
+/*function addTwoIntegers(firstInteger, secondInteger) {
     //console.log(firstInteger + secondInteger)
     // return firstInteger + secondInteger
     if(typeof firstInteger === "number" && typeof secondInteger === "number"){
@@ -6,7 +6,35 @@ function addTwoIntegers(firstInteger, secondInteger) {
     } else{
         return "Valamelyik argumentum nem szám, ezért a művelet nem elvégezhető"
     }
-}
+
+
 console.log( addTwoIntegers("12", 13) );
 console.log( addTwoIntegers(1920, 85) );
-console.log( 0 === "0" );
+console.log( 0 === "0" ); */
+
+function loadEvent(eventObject) {
+    //console.log(eventObject);
+    const rootElement = document.getElementById("root");
+    //console.dir(document);
+    console.dir(rootElement);
+
+    const listOfSectionElements = document.querySelectorAll("section");
+    //console.log(listOfSectionElements);
+
+    //rootElement.classList.add("newClass")
+
+    /*listOfSectionElements[0].classList.add("newClass")
+    listOfSectionElements[1].classList.add("newClass")*/
+
+
+    let anchors = "";
+
+    for (const sectionElement of listOfSectionElements) {
+        //sectionElement.classList.add("newClass");
+        //anchors = anchors + `<a> ${ sectionElement.id } </a>`
+        anchors += `<a href="#${ sectionElement.id }"> ${ sectionElement.id } </a>`;
+    }
+    console.log(anchors);
+    rootElement.insertAdjacentHTML("afterbegin", `<header>${ anchors }</header>`)
+}
+window.addEventListener("load", loadEvent);
